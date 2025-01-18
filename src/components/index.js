97b65ec3fbdc5de503ef407882c9c03dfa3f2020
Sharan Kumar I <i.kumar.sharan@gmail.com>
2025-01-18 10:36:08 +0530

    added user ip location

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import HomeInfo from "./HomeInfo";
 import Contactme from "./Contactme";
+import UserIP from "./UserIp";
 // import ProjectImg from "./ProjectImg"
 // import ProCarousel from './ProCarousel'
 export {
@@ -15,6 +16,7 @@ export {
     Footer,
     HomeInfo,
     Contactme,
+    UserIP,
     // ProjectImg,
     // ProCarousel
 }
