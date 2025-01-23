d2d5d3043ca27039f51fcadc18b0f040a7bb3fc5
Sharan Kumar I <i.kumar.sharan@gmail.com>
2025-01-23 14:11:42 +0530

    Making useripinfo page responsive

--- src/components/UserIP.jsx
+++ src/components/UserIP.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const UserIP = () => {
 //   const [ip, setIp] = useState('');
-const [ipDetails, setIpDetails] = useState([]);
-useEffect(() => { 
-    axios.get('https://ipapi.co/json/').then((res) => { 
-        setIpDetails(res.data); 
-        // setLat(res.data.latitude); 
-        // setLon(res.data.longitude); 
-    }); 
-}, [])
+// const [ipDetails, setIpDetails] = useState([]);
+// useEffect(() => { 
+//     axios.get('https://ipapi.co/json/').then((res) => { 
+//         setIpDetails(res.data); 
+//         // setLat(res.data.latitude); 
+//         // setLon(res.data.longitude); 
+//     }); 
+// }, [])
 
 //   useEffect(() => {
 //     const fetchIP = async () => {
@@ -27,7 +28,7 @@ useEffect(() => {
 
   return (
     <div className='flex items-center text-center'>
-      <p className=' text-red-500 font-light'>Your IP Address: {ipDetails.ip} ({ipDetails.city}, {ipDetails.region}, {ipDetails.country_name})</p>
+      <p className=' text-red-500 font-light'><Link to='/useripinfo'>Click to know your IP Address </Link></p>
     </div>
   );
 };
--- src/pages/UserIPinfo.jsx
+++ src/pages/UserIPinfo.jsx
@@ -12,14 +12,14 @@ useEffect(() => {
     }); 
 }, [])
     return (
-            <div className="bg-white max-w-2xl items-center align-middle shadow overflow-hidden sm:rounded-lg mt-28 mb-28 mx-28">
+            <div className="bg-white max-w-4xl items-center align-middle shadow overflow-hidden sm:rounded-lg md:mx-auto mt-14 mb-28">
     <div className="px-4 py-5 sm:px-6">
         <h3 className="text-lg leading-6 font-medium text-gray-900">
             User Informations
         </h3>
-        {/* <p className="mt-1 max-w-2xl text-sm text-gray-500">
-            Details and informations about user.
-        </p> */}
+        <p className="mt-1 max-w-2xl text-sm text-gray-500">
+            Details and informations are based on your IP address.
+        </p>
     </div>
     <div className="border-t border-gray-200">
         <dl>
@@ -49,18 +49,59 @@ useEffect(() => {
             </div>
             <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt className="text-sm font-medium text-gray-500">
-                    Country
+                    Country with code
                 </dt>
                 <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                    {ipDetails.country_name}
+                    {ipDetails.country_name}, {ipDetails.country_calling_code}
                 </dd>
             </div>
             <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt className="text-sm font-medium text-gray-500">
-                    Internet Service Provider
+                    Postal Code
                 </dt>
                 <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                    {ipDetails.isp}
+                    {ipDetails.postal}
+                </dd>
+            </div>
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                    Time Zone
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    {ipDetails.timezone}
+                </dd>
+            </div>
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                    Latitude
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    {ipDetails.latitude}
+                </dd>
+            </div>
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                    Longitude
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    {ipDetails.longitude}
+                </dd>
+            </div>
+
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                    ASN
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    {ipDetails.asn}
+                </dd>
+            </div>
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                    Internet Service Provider(ISP)
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    {ipDetails.org}
                 </dd>
             </div>
         </dl>
